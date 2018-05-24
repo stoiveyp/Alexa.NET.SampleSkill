3e3a5ff409f2acb51d6ed42b893c0173362099b1
Steven Pears <github@lotsofideas.co.uk>
2018-05-24 08:19:51 +0100

    Initial skill structure (sans tests)

---
+++ src/Alexa.NET.SampleSkill/RequestHandlers/Launch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Alexa.NET.Request.Type;
+using Alexa.NET.RequestHandlers;
+using Alexa.NET.Response;
+
+namespace Alexa.NET.SampleSkill.RequestHandlers
+{
+	public class Launch:Alexa.NET.RequestHandlers.Handlers.SynchronousRequestHandler
+    {
+
+		public override bool CanHandle(RequestInformation information)
+		{
+			return information.SkillRequest.Request is LaunchRequest;
+		}
+
+		public override SkillResponse HandleSyncRequest(RequestInformation information)
+		{
+			return ResponseBuilder.Tell("You launched the skill. Thanks.");
+		}
+	}
+}
---
+++ test/Alexa.NET.SampleSkill.Tests/FunctionTest.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Xunit;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.TestUtilities;
+
+using Alexa.NET.SampleSkill;
+
+namespace Alexa.NET.SampleSkill.Tests
+{
+    public class FunctionTest
+    {
+
+    }
+}
